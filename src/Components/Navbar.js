cc34b7658421d83868f41a2c974796f3234e8d1b
Sameer092 <sameershazad501@gmail.com>
2023-07-25 13:07:42 +0500

    Hosting check

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
     return (
@@ -14,10 +14,10 @@ export default function Navbar(props) {
                     }`}
             >
                 <div className="container-fluid">
-                    <Link className={`navbar-brand ${props.gmode === "success" || props.bmode === "primary" || props.mode === "dark" ? "text-white" : "text-black"
-                        }`} to="/">
+                    <a className={`navbar-brand ${props.gmode === "success" || props.bmode === "primary" || props.mode === "dark" ? "text-white" : "text-black"
+                        }`} href="#">
                         {props.title}
-                    </Link>
+                    </a>
                     <button
                         className="navbar-toggler"
                         type="button"
@@ -32,16 +32,16 @@ export default function Navbar(props) {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className={`nav-link active ${props.gmode === "success" || props.bmode === "primary" || props.mode === "dark" ? "text-white" : "text-black"
-                                    }`} aria-current="page" to="/">
+                                <a className={`nav-link active ${props.gmode === "success" || props.bmode === "primary" || props.mode === "dark" ? "text-white" : "text-black"
+                                    }`} aria-current="page" href="#">
                                     Home
-                                </Link>
+                                </a>
                             </li>
                             <li className="nav-item">
-                                <Link className={`nav-link ${props.gmode === "success" || props.bmode === "primary" || props.mode === "dark" ? "text-white" : "text-black"
-                                    }`} to="/about">
+                                <a className={`nav-link ${props.gmode === "success" || props.bmode === "primary" || props.mode === "dark" ? "text-white" : "text-black"
+                                    }`} href="#">
                                     {props.aboutText}
-                                </Link>
+                                </a>
                             </li>
                         </ul>
                         <form className="d-flex ">
